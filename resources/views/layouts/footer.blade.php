0679df6d8198b9db19fb46f255b84d379bea9193
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-09-07 10:52:18 +0800

    fix

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="py-5 bg-dark">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2025</p></div>
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; 彰化數位教學入口網 {{ date('Y') }} by ET Wang</p></div>
 </footer>
