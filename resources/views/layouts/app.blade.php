1ecc3d467d0431524a6ab704db30871638df385e
wangchifu <wangchifu@gmail.com>
2026-09-17 15:00:59 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />        
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <!-- 載入 Google Identity Services SDK -->
+        <script src="https://accounts.google.com/gsi/client" async defer></script>
     </head>
     <body>
         <!-- Navigation-->
